56d285faed063110e262875744918df8c8c61102
obada almatrami <obada.matrami92@gmail.com>
2019-10-21 18:05:44 +0300

    updateChart

--- js/app.js
+++ js/app.js
@@ -1,194 +1,217 @@
 'use strict';
 
-var imgArr = [
-    ['Bag', 'img/bag.jpg'],
-    ['Banana Cutter', 'img/banana.jpg'],
-    ['Bathroom iPad Stand', 'img/bathroom.jpg'],
-    ['Yellow Boots', 'img/boots.jpg'],
-    ['Multi-function Toaster', 'img/breakfast.jpg'],
-    ['Meatball Bubble Gum', 'img/bubblegum.jpg'],
-    ['Red Chair', 'img/chair.jpg'],
-    ['Cthulhu Toy', 'img/cthulhu.jpg'],
-    ['Doggie Duck Bill', 'img/dog-duck.jpg'],
-    ['Can Of Dragon Meat', 'img/dragon.jpg'],
-    ['Utencil Pen', 'img/pen.jpg'],
-    ['Pet Sweep', 'img/pet-sweep.jpg'],
-    ['Pizza Scissors', 'img/scissors.jpg'],
-    ['Shark Sleeping Bag', 'img/shark.jpg'],
-    ['Baby Sweep Oneside', 'img/sweep.png'],
-    ['Tauntaun Sleeping Bag', 'img/tauntaun.jpg'],
-    ['Can Of Unicorn Meat', 'img/unicorn.jpg'],
-    ['Tentacle USB', 'img/usb.gif'],
-    ['Water Can', 'img/water-can.jpg'],
-    ['Unique Wine Glass', 'img/wine-glass.jpg']
-];
-var imgEl = document.getElementById('left-pic');
-var imgEl2 = document.getElementById('center-pic');
-var imgEl3 = document.getElementById('right-pic');
-var ctx = document.getElementById('chart').getContext('2d');
-
-// Local Storage variables
-var storedImgArr = JSON.parse(localStorage.getItem('img'));
-var storedCounter = JSON.parse(localStorage.getItem('Counter'));
-
-// array to store the objects
-Image.allImages = [];
-Image.justViewed = [];
-Image.pics = [imgEl, imgEl2, imgEl3];
-
-// make an object
-function Image(name, filepath) {
-    this.name = name;
-    this.filepath = filepath;
-    this.votes = 0;
-    this.views = 0;
-    Image.allImages.push(this);
+function BusMall(title, src) {
+    this.title = title;
+    this.src = src;
+    this.clickCtr = 0;
+    this.shownCtr = 0;
+    BusMall.all.push(this);
 }
 
-// create all the Image Objects
-function createImageObjects() {
-    for (var i = 0; i < imgArr.length; i++) {
-        new Image(imgArr[i][0], imgArr[i][1]);
-    }
+BusMall.roundCtr = 0;
+BusMall.roundLimit = 25;
+
+BusMall.all = [];
+
+BusMall.container = document.getElementById('item-container');
+BusMall.leftImage = document.getElementById('leftimage');
+BusMall.centerimage = document.getElementById('centerimage');
+BusMall.rightImage = document.getElementById('rightimage');
+
+BusMall.leftTitle = document.getElementById('lefttitle');
+BusMall.centerTitle = document.getElementById('centertitle');
+BusMall.rightTitle = document.getElementById('righttitle');
+
+BusMall.leftObject = null;
+BusMall.centerObject = null;
+BusMall.rightObject = null;
+
+new BusMall('bag', 'images/bag.jpg');
+new BusMall('banana', 'images/banana.jpg');
+new BusMall('bathroom', 'images/bathroom.jpg');
+new BusMall('boots', 'images/boots.jpg');
+new BusMall('breakfast', 'images/breakfast.jpg');
+new BusMall('bubblegum', 'images/bubblegum.jpg');
+new BusMall('chair', 'images/chair.jpg');
+new BusMall('cthulhu', 'images/cthulhu.jpg');
+new BusMall('dog-duck', 'images/dog-duck.jpg');
+new BusMall('dragon', 'images/dragon.jpg');
+new BusMall('pen', 'images/pen.jpg');
+new BusMall('pet-sweep', 'images/pet-sweep.jpg');
+new BusMall('scissors', 'images/scissors.jpg');
+new BusMall('shark', 'images/shark.jpg');
+new BusMall('sweep', 'images/sweep.png');
+new BusMall('tauntaun', 'images/tauntaun.jpg');
+new BusMall('unicorn', 'images/unicorn.jpg');
+new BusMall('usb', 'images/usb.gif');
+new BusMall('water-can', 'images/water-can.jpg');
+new BusMall('wine-glass', 'images/wine-glass.jpg');
+
+function renderBus() {
+
+    var forbidden = [BusMall.leftObject, BusMall.centerObject, BusMall.rightObject];
+
+    do {
+
+        BusMall.leftObject = getRandomitems();
+
+    } while (forbidden.includes(BusMall.leftObject))
+
+    forbidden.push(BusMall.leftObject);
+
+    do {
+
+        BusMall.centerObject = getRandomitems();
+
+    } while (forbidden.includes(BusMall.centerObject))
+
+    forbidden.push(BusMall.centerObject);
+    do {
+
+        BusMall.rightObject = getRandomitems();
+
+    } while (forbidden.includes(BusMall.rightObject));
+
+
+
+    BusMall.leftObject.shownCtr++;
+    BusMall.centerObject.shownCtr++;
+    BusMall.rightObject.shownCtr++;
+
+    var leftImageElement = BusMall.leftImage;
+    var centerimageElement = BusMall.centerimage;
+    var rightBusMallImageElement = BusMall.rightImage;
+
+    leftImageElement.setAttribute('src', BusMall.leftObject.src);
+    leftImageElement.setAttribute('alt', BusMall.leftObject.title);
+    centerimageElement.setAttribute('src', BusMall.centerObject.src);
+    centerimageElement.setAttribute('alt', BusMall.centerObject.title);
+    rightBusMallImageElement.setAttribute('src', BusMall.rightObject.src);
+    rightBusMallImageElement.setAttribute('alt', BusMall.rightObject.title);
+
+    BusMall.leftTitle.textContent = BusMall.leftObject.title;
+    BusMall.centerTitle.textContent = BusMall.centerObject.title;
+    BusMall.rightTitle.textContent = BusMall.rightObject.title;
 }
-createImageObjects();
 
-// Init the local storage variables (if empty)
-if (storedImgArr === null) {
-    console.log('stored image array is empty');
-    localStorage.setItem('Images', JSON.stringify(Image.allImages));
-    storedImgArr = JSON.parse(localStorage.getItem('Images'));
-}
-if (storedCounter === null) {
-    console.log('stored counter is empty');
-    storedCounter = 0;
-    localStorage.setItem('Counter', JSON.stringify(storedCounter));
-    storedCounter = JSON.parse(localStorage.getItem('Counter'));
+function getRandomitems() {
+    var index = Math.floor(Math.random() * BusMall.all.length);
+    return BusMall.all[index];
 }
 
-// add all the event listeners
-var container = document.getElementById('container');
-container.addEventListener('click', handleClick);
-
-showImages();
-
-function showImages() {
-    var currentlyShowing = [];
-
-    currentlyShowing[0] = getRandomIndex();
-    while (Image.justViewed.indexOf(currentlyShowing[0]) !== -1) {
-        currentlyShowing[0] = getRandomIndex();
-    }
-
-    currentlyShowing[1] = getRandomIndex();
-    while (currentlyShowing[0] === currentlyShowing[1] ||
-        Image.justViewed.indexOf(currentlyShowing[1]) !== -1) {
-        currentlyShowing[1] = getRandomIndex();
-    }
-
-    currentlyShowing[2] = getRandomIndex();
-    while (currentlyShowing[0] === currentlyShowing[2] ||
-        currentlyShowing[1] === currentlyShowing[2] ||
-        Image.justViewed.indexOf(currentlyShowing[2]) !== -1) {
-        currentlyShowing[2] = getRandomIndex();
-    }
-
-    for (var i = 0; i < Image.pics.length; i++) {
-        Image.pics[i].src = storedImgArr[currentlyShowing[i]].filepath;
-        Image.pics[i].id = storedImgArr[currentlyShowing[i]].name;
-        storedImgArr[currentlyShowing[i]].views += 1;
-        Image.justViewed[i] = currentlyShowing[i];
-    }
+// not using this, just showing the better way vs. ceil
+function randomInRange(min, max) {
+    var range = max - min + 1; // add one since we will be flooring
+    var rand = Math.floor(Math.random() * range) + min
+    return rand;
 }
 
-function handleClick(event) {
-    console.log('total clicks: ' + storedCounter);
+function Totalsproducts() {
 
-    if (storedCounter > 24) {
-        storedCounter = 0;
-        document.getElementById('container').removeEventListener('click', handleClick);
-        showTableResults();
+    var tableBody = document.getElementById('content-area');
 
 
-        // reset the local storage
-        localStorage.setItem('Counter', JSON.stringify(storedCounter));
-        for (var i = 0; i < storedImgArr.length; i++) {
-            storedImgArr[i].votes = 0;
-        }
-        localStorage.setItem('Images', JSON.stringify(storedImgArr));
+    tableBody.innerHTML = '';
+
+
+    for (var i = 0; i < BusMall.all.length; i++) {
+        var item = BusMall.all[i];
+        var row = addElement('tr', tableBody);
+        addElement('td', row, item.title);
+        addElement('td', row, '' + item.clickCtr);
+        addElement('td', row, '' + item.shownCtr);
+        addElement('td', row, '' + ' had ' + item.clickCtr + ' votes ' + ' and was shown ' + item.shownCtr);
+
+
+    }
+
+}
+
+function addElement(tag, container, text) {
+    var element = document.createElement(tag);
+    container.appendChild(element);
+    if (text) {
+        element.textContent = text;
+    }
+    return element;
+}
+
+function clickItem(event) {
+
+    var clickedId = event.target.id;
+    var itemClicked;
+
+    if (clickedId === 'leftimage') {
+        itemClicked = BusMall.leftObject;
+    } else if (clickedId === 'centerimage') {
+        itemClicked = BusMall.centerObject;
+    } else if (clickedId === 'rightimage') {
+        itemClicked = BusMall.rightObject;
     } else {
-        if (event.target.id === 'container') {
-            return alert('Nope, you need to click on an image.');
-        }
+        console.log('Um, what was clicked on???', clickedId);
+    }
 
-        storedCounter += 1;
-        localStorage.setItem('Counter', JSON.stringify(storedCounter));
-        for (var i = 0; i < storedImgArr.length; i++) {
-            if (event.target.id === storedImgArr[i].name) {
-                storedImgArr[i].votes += 1;
-                // update the local storage
-                localStorage.setItem('Images', JSON.stringify(storedImgArr));
-                console.log(event.target.id + ' has ' + storedImgArr[i].votes + ' votes in ' + storedImgArr[i].views + ' views');
-            }
-        }
+    if (itemClicked) {
+        itemClicked.clickCtr++;
+        BusMall.roundCtr++;
 
-        showImages();
+        Totalsproducts();
+
+        if (BusMall.roundCtr === BusMall.roundLimit) {
+            rendermallitems();
+            alert('No more clicking for you!');
+
+            BusMall.container.removeEventListener('click', clickItem);
+
+        } else {
+
+            renderBus();
+        }
     }
 }
 
-// show the final results in a table
-function showTableResults() {
-    var allNames = [];
-    var allVotes = [];
-    var labelColors = [];
-
-    // create all the data we need for the chart
-    for (var i = 0; i < storedImgArr.length; i++) {
-        allNames.push(storedImgArr[i].name);
-        allVotes.push(storedImgArr[i].votes);
-        labelColors.push(getRandomColor());
+function rendermallitems() {
+    var MallArray = [];
+    var ClickedArray = [];
+    var shownArray = [];
+    for (let i = 0; i < BusMall.all.length; i++) {
+        var MallInstenc = BusMall.all[i];
+        MallArray.push(MallInstenc.title + ' Vote');
+        MallArray.push(MallInstenc.title + ' Shown');
+        ClickedArray.push(MallInstenc.clickCtr);
+        shownArray.push(MallInstenc.shownCtr);
     }
-
-    var myChart = new Chart(ctx, {
+    var ctx = document.getElementById('Chart').getContext('2d');
+    var chart = new Chart(ctx, {
         type: 'bar',
         data: {
-            labels: allNames,
+            labels: ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck ',
+                'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'
+            ],
             datasets: [{
-                label: '# of Votes',
-                data: allVotes,
-                backgroundColor: labelColors
-            }]
-        },
-        options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true
-                    }
-                }]
-            }
+                    label: 'Item Vote',
+                    backgroundColor: 'Blue',
+                    borderColor: 'black',
+                    data: ClickedArray,
+                },
+                {
+                    label: 'Item Shown',
+                    backgroundColor: 'red',
+                    borderColor: 'black',
+                    data: shownArray,
+                }
+            ],
+            options: {}
         }
     });
 }
 
-/// Helper functions
 
-// this function returns a random number from 0 to the max number of images
-function getRandomIndex() {
-    return Math.floor(Math.random() * Image.allImages.length);
-}
 
-// this function rounds a decimal to n number of places
-function round(value, decimals) {
-    return Number(Math.round(value + 'e' + decimals) + 'e-' + decimals);
-}
 
-// this function creates and returns a random color
-function getRandomColor() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-}
+
+BusMall.container.addEventListener('click', clickItem);
+
+Totalsproducts();
+renderBus();
+rendermallitems();
